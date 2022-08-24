f8e6b591575a830058db09087dea12a1deef12c3
mindofkaka <106789262+mindofkaka@users.noreply.github.com>
2022-08-25 04:33:00 +0700

    Create exam.cpp

---
+++ exam.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	long long x, a, b;
+	cin >> x;
+	if(x == 1){
+		cout << "Hello World";
+	}else{
+		cin >> a >> b;
+		long long c = a*b;
+		cout << c;
+	}
+}
